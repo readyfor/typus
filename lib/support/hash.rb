2a17cc5b7e20cbc484d0a065522e7c587c651e3d
toyoc <toyoc@mac.com>
2018-08-27 17:56:23 +0900

    concern ActionController::Parameters

--- lib/support/hash.rb
+++ lib/support/hash.rb
@@ -10,3 +10,16 @@ class Hash
   end
 
 end
+
+module ActionController
+  class Parameters
+    def compact
+      delete_if { |k, v| v.blank? }
+    end
+
+    def cleanup
+      whitelist = %w(controller action id _input _popup resource attribute)
+      delete_if { |k, v| !whitelist.include?(k) }
+    end
+  end
+end
